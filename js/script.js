46cf72e10b96edac4cc5cbda1efd45cf636cf2c4
YourGuruNet <arvels123@inbox.lv>
2020-11-20 19:10:12 +0200

    Finished desktop version

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
 //Gallery Modal
-/*
-let modal = document.getElementById("galleryModal");
-let img = document.getElementById("onClick");
-let modalImg = document.getElementById("gallery-image-1");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-let span = document.getElementsByClassName("modal__close")[0];
-span.onclick = function () {
-  modal.style.display = "none";
-};*/
 let modal = document.getElementById("galleryModal");
 function myFunction(imgs) {
   modal.style.display = "block";
-  var expandImg = document.getElementById("expandedImg");
+  let expandImg = document.getElementById("expandedImg");
   expandImg.src = imgs.src;
 }
 let span = document.getElementsByClassName("modal__close")[0];
 span.onclick = function () {
   modal.style.display = "none";
 };
+
+// Mobile Nav
+function changeIcon() {
+  let icon = document.getElementById("navIcon");
+  let menu = document.getElementById("navigationMenu");
+  let menuList = document.getElementById("mobileMenu");
+  if (icon.className === "navigation__icon") {
+    icon.className = "navigation__icon--close";
+    menu.className = "nav_menu-mobile";
+    menuList.style.display = "block";
+  } else {
+    icon.className = "navigation__icon";
+    menu.className = "sidebar__container";
+    menuList.style.display = "none";
+  }
+}
+
+//Book Now Modal
+
+function bookNow() {
+  let bookNow = document.getElementById("myModal");
+  let close = document.getElementsByClassName("close")[0];
+
+  bookNow.style.display = "block";
+
+  close.onclick = function () {
+    bookNow.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == bookNow) {
+      bookNow.style.display = "none";
+    }
+  };
+}
